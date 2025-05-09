0c5e93dc86e3aac5ceb302f7319b06eb83f116d8
yuto <gausslaw926@gmail.com>
2025-05-09 22:17:54 +0900

    Add meta keyword settings

--- src/siteConfig/config.ts
+++ src/siteConfig/config.ts
@@ -1,6 +1,14 @@
 export const SITE_THEME_COLOR = "#ffffff";
 export const SITE_NAME = "iGEM Science Tokyo homepage";
-export const SITE_DESCRIPTION = "iGEM Science Tokyoのホームページです";
+export const SITE_DESCRIPTION = "iGEM Science Tokyo は東京科学大学の公認サークルとして、合成生物学を用いた社会課題解決を目指し国際大会iGEMに挑戦する学生チームです。研究内容、チーム体制、最新成果を発信しております。";
 export const SITE_AUTHOR = "iGEM Science Tokyo";
 export const SITE_TWITTER_ID = "@igem_tokyotech";
 export const SITE_OGP_URL = "/ogp.png";
+export const SITE_KEYWORDS = [
+  "iGEM",
+  "Science Tokyo",
+  "合成生物学",
+  "国際大会",
+  "東京工業大学",
+  "東京科学大学"
+];
